7a0bed0d09e28fe1389d687d0ab0f7a547651196
Mario Pella <pellamario18@gmail.com>
2026-01-28 00:39:56 +0100

    Inizializzazione progetto

---
+++ src/main/java/com/workfit/backend/WorkfitAppBackedApplication.java
@@ -0,0 +1,13 @@
+package com.workfit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkfitAppBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkfitAppBackedApplication.class, args);
+	}
+
+}
